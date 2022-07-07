d5f7c8480c0093deef1fdf2e81e02350e6e75102
Serje <Serje.havandjian@gmail.com>
2022-07-07 14:46:19 -0700

    commit tree b0dab2de0ae197ee1ee96ba3f9d437aa6dd9857f

--- lib/queue_from_stack.rb
+++ lib/queue_from_stack.rb
@@ -1,3 +1,14 @@
 require_relative './stack'
 
-# your code here
+class MyQueue
+
+    def initialize
+        @s1=Stack.new
+        @s2=Stack.new
+    end
+
+    def add(value)
+       self.peek
+    end
+
+end
